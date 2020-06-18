5c8541546213618d4e3fabd192959febcd03c576
Julian Giertsen da Cunha <juliangdacunha@gmail.com>
2020-06-18 13:15:11 +0200

    modified serachbar

--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -1,30 +1,30 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
-  constructor(props) {
-    super(props);
+  // constructor(props) {
+  //   super(props);
 
-    this.state = {
-      term: ""
-    };
-  }
+  //   this.state = {
+  //     term: ""
+  //   };
+  // }
 
   shouldComponentUpdate() {
     return false;
   }
 
-    handleChange = (event) => {
-    this.setState({
-      term: event.target.value
-    });
+  handleChange = (event) => {
+    // this.setState({
+    //   term: event.target.value
+    // });
     this.props.search(event.target.value);
   }
 
+  // value={this.state.term} - would be part of input tag in render method
 
   render() {
     return (
       <input
-        value={this.state.term}
         type="text"
         className="form-control form-search"
         onChange={this.handleChange}
